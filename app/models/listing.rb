d51d11e59b73dae1fcf0f2cebcbff624ccf1534f
Eric Parker <ericparker@Erics-MacBook-Pro-2.local>
2016-11-27 19:15:51 -0500

    Do not include images on local host in git

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,11 @@
 class Listing < ActiveRecord::Base
-    has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "no-image-available-md.png",
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    if Rails.env.development?
+        has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "no-image-available-md.png"
+    else
+
+        has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "no-image-available-md.png",
+        :storage => :dropbox,
+        :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+        :path => ":style/:id_:filename"
+    end
 end
