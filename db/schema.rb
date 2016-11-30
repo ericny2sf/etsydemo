6ad64f60a64b84d97d6af160f2e33fb49662be4a
Eric Parker <ericparker@Erics-MacBook-Pro-2.local>
2016-11-29 21:53:53 -0500

    Attach listings to a user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129013046) do
+ActiveRecord::Schema.define(version: 20161130014425) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20161129013046) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
